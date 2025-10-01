4e213552b567f624fe25a449c3277c4e233062cc
eticialima <leticialimacgi@gamil.com>
2025-10-01 00:26:43 -0300

    ajusta css footer e home page

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core'; 
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
